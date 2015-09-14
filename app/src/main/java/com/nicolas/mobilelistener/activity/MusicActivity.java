2b6b5dae860a6cd0e6c56cbbe9122e0ded036f25
liwangadd <liwangadd@gmail.com>
2015-09-14 11:23:56 +0800

    add activities

---
+++ app/src/main/java/com/nicolas/mobilelistener/activity/MusicActivity.java
@@ -0,0 +1,19 @@
+package com.nicolas.mobilelistener.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.nicolas.mobilelistener.R;
+
+/**
+ * Created by Nikolas on 2015/9/14.
+ */
+public class MusicActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.musiclis_activityt);
+    }
+
+}
---
+++ app/src/main/java/com/nicolas/mobilelistener/activity/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.nicolas.mobilelistener.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.nicolas.mobilelistener.R;
+
+/**
+ * Created by Nikolas on 2015/9/14.
+ */
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
